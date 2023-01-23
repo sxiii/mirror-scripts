22029f76b648061a626750c7ac175be205c932f1
Dennis Ivanov <securityxiii@gmail.com>
2023-01-23 15:56:47 +0100

    Add files via upload

---
+++ step1-docker-certbot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo docker run -it --rm --name certbot \
+            -v "/etc/letsencrypt:/etc/letsencrypt" \
+            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+            -p "80:80/tcp" \
+            certbot/certbot certonly --non-interactive --agree-tos --standalone -m norwaymeta@gmail.com --domains your.website.com
