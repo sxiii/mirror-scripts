3f9acc15913513e610453fc61412578b51986fd5
Dennis Ivanov <securityxiii@gmail.com>
2023-01-24 09:13:24 +0100

    Update step1-docker-certbot.sh

--- step1-docker-certbot.sh
+++ step1-docker-certbot.sh
@@ -3,4 +3,4 @@ sudo docker run -it --rm --name certbot \
             -v "/etc/letsencrypt:/etc/letsencrypt" \
             -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
             -p "80:80/tcp" \
-            certbot/certbot certonly --non-interactive --agree-tos --standalone -m norwaymeta@gmail.com --domains your.website.com
+            certbot/certbot certonly --non-interactive --agree-tos --standalone -m your@email.com --domains your.website.com
